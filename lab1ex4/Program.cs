fec8a83d2787410570ab0bb5d185696f84d509a7
Alex Filip <alexfilippaul@gmail.com>
2022-11-27 17:43:45 +0200

    semnul numarului citit

---
+++ lab1ex4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab1ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //semnul numarului introdus(+,-,0)
+            int a;
+            Console.WriteLine("Introduceti un numar: ");
+            a = int.Parse(Console.ReadLine());
+            if (a > 0) Console.WriteLine("+");
+            else if (a < 0) Console.WriteLine("-");
+            else Console.WriteLine("0");
+        }
+    }
+}
